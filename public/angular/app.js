706cb585fa811a1955756d3432d10b8a4fbddfee
TangHao1987 <mythabc1987@gmail.com>
2015-07-20 18:01:21 +0800

    simplified the index page

--- public/angular/app.js
+++ public/angular/app.js
@@ -1,6 +1,25 @@
 /**
  * Created by tang.hao on 20/7/2015.
  */
-angular.module('app', []).controller('mainController', ['$scope', function($scope){
+angular.module('app', []).controller('mainController', ['$scope', '$http', function($scope, $http){
     $scope.title = 'Wood Scent';
+    $http.get("config/navItem.json").success(function(data, status, headers, config) {
+        $scope.navItems = data;
+    });
+
+    $http.get("config/service.json").success(function(data, status, headers, config) {
+        $scope.service = data;
+    });
+
+    $http.get("config/portfolio.json").success(function(data, status, headers, config) {
+        $scope.portfolio = data;
+    });
+
+    $http.get("config/timeline.json").success(function(data, status, headers, config) {
+        $scope.timeline = data;
+    });
+
+    $http.get("config/team.json").success(function(data, status, headers, config) {
+        $scope.team = data;
+    });
 }]);
